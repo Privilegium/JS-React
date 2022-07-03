3d6a47ca7cafd03513b472325ab5b5f38055473d
Sviatoslav <opryshkosm@icloud.com>
2022-07-03 23:24:53 +0300

    first hw on the course

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+const numberOfFilms = prompt('How much movies you have watched yet?');
+
+const lastMovie = prompt('Name one of the latest watched movies?');
+const lastMovieRate = prompt('Type a last watched movie rating:');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        lastMovie: lastMovieRate,
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
