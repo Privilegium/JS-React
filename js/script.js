d30b9ddc49c44c5eaeb1e7d2dc518afa9b3c5120
Sviatoslav <opryshkosm@icloud.com>
2022-07-03 23:37:15 +0300

    first hw correction

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ const lastMovieRate = prompt('Type a last watched movie rating:');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {
-        lastMovie: lastMovieRate,
+        // lastMovie: lastMovieRate,
     },
     actors: {},
     genres: [],
     privat: false
 };
 
+personalMovieDB.movies['' + lastMovie] = lastMovieRate;
+
+ console.log(personalMovieDB);
 
