21e5fcb646e07d983f66c9fd96c493a1de495161
Sviatoslav <opryshkosm@icloud.com>
2022-07-06 14:44:11 +0300

    first function practice

--- js/PRACTICE_MOVIES.js
+++ js/PRACTICE_MOVIES.js
@@ -1,18 +1,66 @@
-'use strict';
+"use strict";
 
-const numberOfFilms = +prompt('How much movies you have watched yet?');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How much movies you have watched yet?");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How much movies you have watched yet?");
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
 };
 
-const a = prompt('Name one of the latest watched movies?'),
-      b = prompt('Type a last watched movie rating:');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Name one of the latest watched movies?"),
+              b = prompt("Type a last watched movie rating:");
+                  
+        if (a != '' && a != null && a.length < 50 && b != '' && b != null) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');             
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }    
+}
+rememberMyFilms();
 
-personalMovieDB.movies[a] = b;
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('You have watched a little amount of movies');
+    } else if (personalMovieDB.count >+ 10 && personalMovieDB.count <= 30) {
+        console.log('You are classic viewer');
+    } else if (personalMovieDB.count > 30) {
+        console.log('You are movie buff');
+    } else {
+        console.log('Error');
+    }    
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favourite genre number ${i}`);
+    }
+}
+writeYourGenres();
+
+
---
+++ js/TEST_FUNCTIONS.js
@@ -0,0 +1,35 @@
+// 'use strict';
+
+// //TEST_6------------------------------------------------------------------------
+
+// // Место для первой задачи
+// function sayHello(name) {
+//     return `Hello ${name}`;
+// }
+
+// // Место для второй задачи
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+// returnNeighboringNumbers(5);
+
+// // Место для третьей задачи
+// function getMathResult(number, progression) {
+//     if (typeof progression !== 'number' || progression <= 0) {
+//         return number;
+//     }
+    
+//     let result = '';
+
+//     for (let i = 1; i <= progression; i++) {
+//             if (i === progression) {
+//                 result += `${number * i}`;
+//             } else {
+//                 result += `${number * i}---`;
+//             }
+//         }
+//     return result;
+//     }
+// console.log(getMathResult(3, 3));
+
+// // TEST_7------------------------------------------------------------------------
