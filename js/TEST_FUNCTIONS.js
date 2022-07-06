279e7915b3a8d5cdd0e3988fe72d8c6816f2d0f8
Sviatoslav <opryshkosm@icloud.com>
2022-07-07 01:17:28 +0300

    test seven functions

--- js/TEST_FUNCTIONS.js
+++ js/TEST_FUNCTIONS.js
@@ -33,3 +33,47 @@
 // console.log(getMathResult(3, 3));
 
 // // TEST_7------------------------------------------------------------------------
+ 
+// Место для первой задачи
+// function calculateVolumeAndArea(oneside) {
+//     const volume = oneside * oneside * oneside;
+//     const area = 6 * (oneside * oneside);
+
+//     if (typeof oneside == 'number' && oneside > 0) {
+//         console.log(`Объем куба: ${volume}, прощадь всей поверхности: ${area}`);
+//     } else {
+//         console.log('При обчисленні сталася помилка');
+//     }
+// }
+// Место для второй задачи
+function getCoupeNumber(sit) {
+    // if (typeof sit !== 'number' || sit < 0 || sit % 2 === 1) {
+    //     console.log('Ошибка. Проверьте правильность введенного номера места');
+    // } else if (sit === 0 || sit > 36) {
+    //     console.log('Таких мест в вагоне не существует');
+    // }
+
+    const coupeSits = {
+        1: [1, 2, 3, 4],
+        2: [5, 6, 7, 8],
+        3: [9, 10, 11, 12],
+        4: [13, 14, 15, 16],
+        5: [17, 18, 19, 20],
+        6: [21, 22, 23, 24],
+        7: [25, 26, 27, 28],
+        8: [29, 30, 31, 32],
+        9: [33, 34, 35, 36]
+    };
+    for (let i = 1; i < 10; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (coupeSits[i][j] == sit) {
+                console.log(i);
+            } else if (typeof sit !== 'number' || sit < 0 || sit % 2 === 1) {
+                console.log('Ошибка. Проверьте правильность введенного номера места');
+            } else if (sit === 0 || sit > 36) {
+                console.log('Таких мест в вагоне не существует');
+            }
+        }
+    }
+}
+getCoupeNumber('12');
